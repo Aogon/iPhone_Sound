60ee0d522f830b664d1ddd488c50868e461232c7
Aogon <aoi_sakai@icloud.com>
2021-02-06 08:59:47 +0900

    Initial Commit

---
+++ Sound/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sound
+//
+//  Created by Sakai Aoi on 2021/02/06.
+//  Copyright © 2021 Sakai Aoi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
